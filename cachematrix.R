57ca5d36cd1ec464724aa9d8584a111e26dabe6f
physicsNE <mcguirk@mit.edu>
2015-05-24 14:49:33 -0400

    full assignment including successful test cases
    makeCacheMatrix defines a list of cache manipulation functions.
    cacheSolve is used to return the inverse of a  matrix, either from cache
    or by applying the solve function. Three test cases are run. 1) new
    matrix, no cache. 2) same matrix, inverse returned from cache. 3) new
    matrix, but cache has inverse for a different matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,71 @@
 ## The first function, 'makeCacheMatrix' creates a vector of four functions as in the makeVector example.
 ## The functions are named setM, getM, setInv, and getInv. 
-## We could have used the same names, but adding M for Matrix makes the names more descriptive,
+## We could have reused the makeVector function names, but adding M for Matrix makes the names more descriptive,
 ## as does changing setmean and getmean to setInv and getInv for the inverse matrices.
-## functions do
+## 
+## The second function 'cacheSolve' checks:
+##     1) if there is an inverse matrix stored, 
+##     2) if the stored matrix is the same matrix we want to Solve,
+## If conditions 1 and 2 are both true, cacheSolve retrieves the inverse or solution matrix.
 
-### Programmed by Michael McGuirk 5/21/2015
+### Programmed by Michael McGuirk 5/21-24/2015
 ### We use two separate matrices: one to cache the inverse,  
 ###   the second to test that we do not use a stored inverse if the input matrix has changed.
 
 testM<-array(c(2,3,4,5,0,7,8,23,10), dim=c(3,3))        ## det(testM) = 156
 testMatrix<-array(c(2,3,4,5,0,7,8,9,10), dim=c(3,3))    ## det(testMatrix) = 72
 
-## Write a short comment describing this function
+## usage: cacheNAME<-makeCacheMatrix(x)
+##        cacheNAME  is any name you choose to give the cache
+##        x is the name of the matrix whose inverse you want cached
+## prupose: makeCacheMatrix creates a list of four functions used to manipulate a cache 
+##          that is a list (matrix, Inverse)
 
-makeCacheMatrix <- function(x = matrix()) {             ## substitute the name of the matrix to cache for x
-  Inv <- NULL                                           ## start with NULL as the Inv (Inverse)
-  setM <- function(y) {                                 
-    x <<- y
-    Inv <<- solve(y)
+makeCacheMatrix <- function(x = matrix()) {      ## substitute the name of the matrix to cache for x
+  Inv <- NULL                                    ## initialize the Inv (Inverse) to NULL
+  setM <- function(x) {                          ## function call will be cacheNAME$set(y)
+                                                 ##    where y is a matrix from any environment (global)
+    x <<- x                                      ## creates a two element list(x,Inv) = (matrix,Inverse)
+    Inv <<- solve(x)                             ## stores the list in the function environment, not global
   }
-  getM <- function() x
-  setInv <- function(Inv) Inv <<- solve(y)
-  getInv <- function() Inv
-  list(setM = setM, getM = getM,
-       setInv = setInv,
+  getM <- function() x                           ## cacheNAME$getM retrieves the matrix from the function environment
+  setInv <- function(Inv) Inv <<- solve(x)       ## cacheNAME$setInv stores Inv in the function envronment
+  getInv <- function() Inv                       ## cacheNAME$getInv retrieve Inv from the function envronment
+  list(setM = setM, getM = getM,                 ## places the four functions in a list  
+       setInv = setInv,                          ##      so they can be accessed by the cacheSolve function
        getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## usage: mInv<-cacheSolve(cacheNAME, mNAME)
+## Return the inverse of matrix mNAME
+##   from the cache if it is stored in cacheNAME
+##   else compute the inverse, store the matrix and inverse in the cache, 
+##        and return the inverse in mInv
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(cacheNAME, mNAME=mNAME) {
+        
+  Inv <- cacheNAME$getInv()                     ## retrieve the Inverse in the cache
+  if(!is.null(Inv)) {                           ## test that an inverse is cached (is not NULL)
+    message("getting cached data")
+    data <- cacheNAME$getM()                    ## retrieve the cached matrix
+    if (isTRUE(all.equal(data,mNAME))) return(Inv) else message("different matrix cached")                              ## if matrices are equal, return cached Inv
+  }                                      
+  message("caching new data")                   ## if not equal, update the cache
+  cacheNAME$setM(mNAME)                         ##    cache the new matrix
+  Inv <- solve(mNAME)                           ##    calculate its inverse
+  cacheNAME$setInv(Inv)                         ##    cache the inverse
+  return(Inv)                                   ## return the inverse of mNAME
+}                                               ## prefer "return(Inv)" over just "Inv" for clarity
+
+## testing the functions
+
+cacheTEST1<-makeCacheMatrix(testM)              ## sets up the cache functions for matrix testM
+mInv<-cacheSolve(cacheTEST1, testM)             ## caches testM and its inverse
+print(mInv)                                     ## prints the inverse that it cached
+mInv<-cacheSolve(cacheTEST1, testM)             ## finds the inverse was cached and returns it 
+print("cached inverse")
+print(mInv)                                     ## prints the inverse that it found
+mInv<-cacheSolve(cacheTEST1, testMatrix)        ## changed vector doesn't return the old cached inverse
+print(mInv)
+## ends the test
